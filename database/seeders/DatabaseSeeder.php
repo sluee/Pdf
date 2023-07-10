5403f5d8babb93c32bd02d177e809df7df420d61
lentrix <hawkmanlentrix@gmail.com>
2023-07-10 09:14:16 +0800

    update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Benjie Lenteria',
+            'email' => 'me@lentrix-dev.com',
+            'password' => bcrypt('aa')
+        ]);
+
+        $this->call(ClientSeeder::class);
+        $this->call(TransactionSeeder::class);
     }
 }
